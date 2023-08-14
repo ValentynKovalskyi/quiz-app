449309c81dc7818701019189b56897e2ff3698ef
Valentyn <kovalsky.valentine@gmail.com>
2023-08-14 17:56:26 +0300

    Created raw vue-vite project

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const env = process.env;
-const database = mysql.createConnection({
+const databaseConnection = mysql.createConnection({
     host: env.DB_HOST,
     user: env.DB_USER,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
 });
 
-module.exports = database;
+module.exports = databaseConnection;

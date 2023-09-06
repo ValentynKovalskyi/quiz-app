550006cb8f70dc0a6c391658f624c9d5f68cb5a2
Valentyn <kovalsky.valentine@gmail.com>
2023-09-06 11:58:32 +0300

    Change font size of inputs

--- client/quiz-app/src/views/SignUpPage.vue
+++ client/quiz-app/src/views/SignUpPage.vue
@@ -24,11 +24,11 @@ import NavBar from '../components/pageblocks/NavBar.vue';
         <NavBar></NavBar>
         <div class="flex flex-col justify-center items-center bg-dark-green w-1/4 shadow-xl shadow-dark rounded-xl">
             <h2 class="font-bold text-white text-center text-xl p-4">Sign Up</h2>
-            <input type="text" v-model="username" placeholder="Username" class="rounded-md shadow-inner shadow-dark w-1/1.5 p-1 mb-1/20">
-            <input type="email" v-model="email" placeholder="E-mail" class="rounded-md shadow-inner shadow-dark w-1/1.5 p-1 mb-1/20">
-            <input type="password" v-model="password" placeholder="Password" class="rounded-md shadow-inner shadow-dark w-1/1.5 p-1 mb-1/20">
-            <input type="password" v-model="passwordConfirmation" placeholder="Confirm Password" class="rounded-md shadow-inner shadow-dark w-1/1.5 p-1 mb-1/20">
-            <CommonButton class="w-1/3 self-center text-base mb-1/10">Sign Up</CommonButton>
+            <input type="text" v-model="username" placeholder="Username" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/20">
+            <input type="email" v-model="email" placeholder="E-mail" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/20">
+            <input type="password" v-model="password" placeholder="Password" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/20">
+            <input type="password" v-model="passwordConfirmation" placeholder="Confirm Password" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/20">
+            <CommonButton class="w-1/3 self-center text-sm mb-1/10">Sign Up</CommonButton>
         </div>
     </div>
 </template>

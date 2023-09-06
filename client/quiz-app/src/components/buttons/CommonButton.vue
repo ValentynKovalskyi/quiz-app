e5521bd4121adcce980e8b3be1dcf9d480864272
Valentyn <kovalsky.valentine@gmail.com>
2023-09-06 11:52:15 +0300

    Fix navbar flexbox

--- client/quiz-app/src/components/buttons/CommonButton.vue
+++ client/quiz-app/src/components/buttons/CommonButton.vue
@@ -1,9 +1,5 @@
 <script>
-    export default {
-        props: {
-            buttonText: String
-        }
-    }
+    export default {}
 </script>
 <template>
      <button class=" bg-green border-2 shadow-semi-dark shadow-xl transition-all text-white rounded-lg font-sans font-bold pr-3 pl-3 pt-1 pb-1

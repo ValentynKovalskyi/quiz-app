334287cc0684eafe954279bb9a8a31eb9d8cbbbc
Valentyn <kovalsky.valentine@gmail.com>
2023-09-09 19:15:33 +0300

    Start develop password strength evaluation signup

---
+++ client/quiz-app/src/utils/passwordStrength.js
@@ -0,0 +1,10 @@
+const passwordLevels = new Map()
+.set(20, 'Weak')
+.set(40, 'Medium')
+.set(80, 'Strong')
+.set(100, 'Impossible');
+
+ function getPasswordStrength ( password: String ) {
+    let reliability = 0;
+    if(password.includes())
+ }
--- client/quiz-app/src/views/SignUpPage.vue
+++ client/quiz-app/src/views/SignUpPage.vue
@@ -9,6 +9,8 @@ import NavBar from '../components/pageblocks/NavBar.vue';
             email: '',
             password: '',
             passswordConfirmation: '',
+            passwordStrengthMessage: '',
+            passwordStrength: 0,
         };
     },
     components: { CommonButton, NavBar },
@@ -24,10 +26,16 @@ import NavBar from '../components/pageblocks/NavBar.vue';
         <NavBar></NavBar>
         <div class="flex flex-col justify-center items-center bg-dark-green w-1/4 shadow-xl shadow-dark rounded-xl">
             <h2 class="font-bold text-white text-center text-xl p-4">Sign Up</h2>
-            <input type="text" v-model="username" placeholder="Username" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/20">
-            <input type="email" v-model="email" placeholder="E-mail" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/20">
-            <input type="password" v-model="password" placeholder="Password" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/20">
-            <input type="password" v-model="passwordConfirmation" placeholder="Confirm Password" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/20">
+            <input type="text" v-model="username" placeholder="Username" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/10">
+            <input type="email" v-model="email" placeholder="E-mail" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/10">
+            <div class="max-h-1/10 flex-col p-1 w-1/1.5 hidden">
+                <span class = "m-1 text-sm text-white"> {{ passwordStrengthMessage }}</span>
+                <div class="h-1 w-full bg-white rounded-2xl">
+                    <div class="h-1 w-1/2 bg-light-blue rounded-2xl"></div>
+                </div>
+            </div>
+            <input type="password" v-model="password" placeholder="Password" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/10">
+            <input type="password" v-model="passwordConfirmation" placeholder="Confirm Password" class="rounded-md shadow-inner shadow-dark text-sm w-1/1.5 p-1 mb-1/10">
             <CommonButton class="w-1/3 self-center text-sm mb-1/10">Sign Up</CommonButton>
         </div>
     </div>
